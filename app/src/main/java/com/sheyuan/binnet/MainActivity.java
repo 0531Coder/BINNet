e3af9954f29519772d3016ece51b4ef2f7efbbe2
liubin <liubin@sheyuan.com>
2017-09-12 15:51:29 +0800

    权限管理完成

--- app/src/main/java/com/sheyuan/binnet/MainActivity.java
+++ app/src/main/java/com/sheyuan/binnet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sheyuan.binnet;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,19 +9,28 @@ import android.widget.TextView;
 import com.sheyuan.baselibrary.BaseRxActivity;
 import com.sheyuan.baselibrary.BasicObserver;
 import com.sheyuan.baselibrary.RetrofitSingleton;
+import com.sheyuan.baselibrary.rxpermissions.Permission;
+import com.sheyuan.baselibrary.rxpermissions.RxPermissions;
 import com.sheyuan.baselibrary.utils.helper.RxSchedulers;
 import com.sheyuan.binnet.response.TransportResponse;
 import com.sheyuan.binnet.service.LoginService;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+
 
 public class MainActivity extends BaseRxActivity {
 
     private TextView tv_result;
     private Button btn_query;
+    private Button btn_call;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RxPermissions rxPermissions = new RxPermissions(this);
+        btn_call = (Button) findViewById(R.id.btn_call);
         tv_result = (TextView) findViewById(R.id.tv_result);
         btn_query = (Button) findViewById(R.id.btn_query);
         btn_query.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +42,29 @@ public class MainActivity extends BaseRxActivity {
                         tv_result.setText(response.getNu());
                     }
                 });
+            }
+        });
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+        rxPermissions.requestEach(Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_CALENDAR,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_SMS,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.SEND_SMS).subscribe(new Consumer<Permission>() {
+            @Override
+            public void accept(@NonNull Permission permission) throws Exception {
+               
             }
         });
     }
